cfbb6d932cbd23931b5477ee57729ee10fa5c678
NguyenHoangQuan2503 <nhquan.fit.hcmus@gmail.com>
2017-04-15 02:22:55 +0700

    change somthing and add blog.html

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /**
  * Created by meo on 4/12/2017.
  */
-var express =require("express");
-var app=express();
+var express = require("express");
+var app = express();
 app.use("/images", express.static(__dirname + '/images'));
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/fonts", express.static(__dirname + '/fonts'));
@@ -11,11 +11,15 @@ app.use("/albums.html", express.static(__dirname + '/albums.html'));
 app.use("/about.html", express.static(__dirname + '/about.html'));
 app.use("/blog.html", express.static(__dirname + '/blog.html'));
 app.use("/home.html", express.static(__dirname + '/home.html'));
-var path=require("path") //assuming express is installed
+app.use("/ilovefoodblog.html", express.static(__dirname + '/ilovefoodblog.html'));
+app.use("/nhatrangblog.html", express.static(__dirname + '/nhatrangblog.html'));
+app.use("/quynhoncityblog.html", express.static(__dirname + '/quynhoncityblog.html'));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname+"/home.html");
+var path = require("path") //assuming express is installed
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/home.html");
 });
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("connect");
 })
