acb11e731ce5929ba5f5f4e4b2f7006a631ed88f
meo bach <meo bach>
2017-04-15 21:22:57 +0700

    fix error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/albums.html", express.static(__dirname + '/albums.html'));
 app.use("/about.html", express.static(__dirname + '/about.html'));
 app.use("/blog.html", express.static(__dirname + '/blog.html'));
 app.use("/home.html", express.static(__dirname + '/home.html'));
-<<<<<<< HEAD
+
 var path=require("path") //assuming express is installed
 //connect database
 var flag=true;
@@ -102,18 +102,18 @@ app.post('/postdata', urlencodedParser, function (req, res) {
 
 
 ///////////////////////////
-app.get('/', function(req, res){
-    res.sendFile(__dirname+"/home.html");
-=======
-app.use("/ilovefoodblog.html", express.static(__dirname + '/ilovefoodblog.html'));
-app.use("/nhatrangblog.html", express.static(__dirname + '/nhatrangblog.html'));
-app.use("/quynhoncityblog.html", express.static(__dirname + '/quynhoncityblog.html'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/home.html");
 
+    app.use("/ilovefoodblog.html", express.static(__dirname + '/ilovefoodblog.html'));
+    app.use("/nhatrangblog.html", express.static(__dirname + '/nhatrangblog.html'));
+    app.use("/quynhoncityblog.html", express.static(__dirname + '/quynhoncityblog.html'));
+});
 var path = require("path") //assuming express is installed
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/home.html");
->>>>>>> 0e88b2f02946f38ae39aba8f968677c42da580f9
+
 });
 app.listen(3000, function () {
     console.log("connect");
